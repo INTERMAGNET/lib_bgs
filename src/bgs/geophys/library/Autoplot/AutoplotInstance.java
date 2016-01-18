6ea9de0a77224cc74186359b61fde31be6ee1740
smf@AD.NERC.AC.UK <smf@AD.NERC.AC.UK>
2016-01-18 17:40:26 +0000

    Updates to the Autoplot handling code

--- src/bgs/geophys/library/Autoplot/AutoplotInstance.java
+++ src/bgs/geophys/library/Autoplot/AutoplotInstance.java
@@ -6,6 +6,7 @@
 package bgs.geophys.library.Autoplot;
 
 import bgs.geophys.library.Data.Iaga2002;
+import bgs.geophys.library.Data.ImagCDF.ImagCDF;
 import bgs.geophys.library.Threads.RunClass;
 import bgs.geophys.library.Threads.ThreadMultiStreamReader;
 import java.io.BufferedInputStream;
@@ -15,6 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * a class to control a running autoplot instance
@@ -136,7 +138,10 @@ public class AutoplotInstance
         autoplot_socket.close();
     }
     
-    /** load a single IAGA-2002 file (without aggregation)
+    /** load a single IAGA-2002 file (without aggregation). This requires reading
+     * the IAGA2002 file so that Autoplot can be instructed how to load it - it
+     * would be nicer if there was a way Autoplot could understand how to read it
+     * without the overhead of having to read it to get metadata first.
      * 
      * @param iaga_2002_file the file to load
      * @param reset true to reset autoplot before plotting the data
@@ -150,8 +155,8 @@ public class AutoplotInstance
         FileInputStream is = new FileInputStream (iaga_2002_file);
         Iaga2002 iaga_2002 = Iaga2002.read (is);
         is.close();
-        
-        String title = "Geomagnetic observatory data for " + iaga_2002.getStationName() + " (" + iaga_2002.getStationCode() + ")";
+
+        String title = makeTitle(iaga_2002.getStationName(), iaga_2002.getStationCode(), (int) iaga_2002.getSamplePeriod());
         String component_codes = iaga_2002.getOriginalComponentCodes();
         int n_header_lines = iaga_2002.getNHeaderLines();
         if (reset) sendCommand ("reset ()");
@@ -210,6 +215,22 @@ public class AutoplotInstance
         }
     }
     
+    /** load an ImagCDF file (with aggregation).
+     * 
+     * @param folder the folder to load data from
+     * @param iaga_code the station code for the data to load
+     * @param start_date the first piece of data to show
+     * @param sample_period the sample period of the data to look for
+     * @param reset true to reset autoplot before plotting the data
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public void loadImagCDF (File folder, String iaga_code, Date start_date, ImagCDF.FilenameSamplePeriod sample_period, boolean reset) 
+    throws FileNotFoundException, IOException
+    {
+        
+    }
+            
     /** send a command to autoplot
      * 
      * @param command the command, in Jython
@@ -271,6 +292,31 @@ public class AutoplotInstance
         } while (time_waiting_ms < AUTOPLOT_RX_TIMEOUT);
 
         throw new IOException ("Timeout waiting for response from autoplot");
+    }
+
+    private String makeTitle (String obsy_name, String obsy_code, int sample_period)
+    {
+        String title = "Geomagnetic observatory data";
+        if (obsy_name == null)
+        {
+            if (obsy_code != null) title += " for" + obsy_code;
+        }
+        else
+        {
+            if (obsy_code != null) title += " for " + obsy_name + " (" + obsy_code + ")";
+            else title += " for" + obsy_name;
+        }
+        switch (sample_period)
+        {
+            case 1000: title += ", 1-second period"; break; 
+            case 60000: title += ", 1-minute period"; break;
+            case 3600000: title += " 1-hour period"; break;
+            default:
+                if (sample_period > 0)
+                    title += Integer.toString (sample_period / 1000) + "s period";
+                break;
+        }
+        return title;
     }
     
 }
